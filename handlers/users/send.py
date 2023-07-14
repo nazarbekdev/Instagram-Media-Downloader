a23ce840cdfdce6936161687e047e16895669a8b
Kabulov-Nazarbek <nazarbekqobulov28@gmail.com>
2023-07-14 10:55:53 +0500

    deploy

---
+++ handlers/users/send.py
@@ -0,0 +1,30 @@
+from loader import bot, dp
+
+from aiogram import types
+from aiogram.dispatcher.filters import Text
+from insta import instadownload
+
+
+@dp.message_handler(Text(startswith='https://www.instagram.com/'))
+async def send_media(message: types.Message):
+    await message.answer('⏳')
+    link = message.text
+    data = instadownload(link=link)
+    if data == 'No':
+        await message.answer("Bu havola orqali hech narsa topilmadi")
+    else:
+        if data['type'] == 'image':
+            await message.answer_photo(photo=data['media'])
+        elif data['type'] == 'video':
+            await message.answer_video(video=data['media'])
+        elif data['type'] == 'carousel':
+            for i in data['media']:
+                await message.answer_document(document=i)
+        elif data['type'] == 'story-video':
+            await message.answer_video(video=data['media'])
+        elif data['type'] == 'story-image':
+            await message.answer_photo(photo=data['media'])
+        else:
+            await message.answer('Bu havola orqali hech narsa topilmadi')
+
+
---
+++ insta.py
@@ -0,0 +1,58 @@
+import json
+import requests
+
+
+def instadownload(link):
+
+    url = "https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index"
+
+
+    querystring = {"url": link}
+
+    headers = {
+	"X-RapidAPI-Key": "030ebe6a65mshe2a17998d98b39fp1dc985jsnb815065b042a",
+	"X-RapidAPI-Host": "instagram-downloader-download-instagram-videos-stories.p.rapidapi.com"
+}
+
+    response = requests.get(url, headers=headers, params=querystring)
+    rest = json.loads(response.text)
+    dict = {}
+
+    if 'Type' in rest.keys():
+        if rest['Type'] == 'Post-Video':
+            dict['type'] = 'video'
+            dict['media'] = rest['media']
+            return dict
+        elif rest['Type'] == 'Post-Image':
+            dict['type'] = 'image'
+            dict['media'] = rest['media']
+            return dict
+        elif rest['Type'] == 'Carousel':
+            dict['type'] = 'carousel'
+            dict['media'] = rest['media']
+            return dict
+        elif rest['stories'][0]['Type'] == 'Story-Video':
+            dict['type'] = 'story-video'
+            dict['media'] = rest['stories'][0]['media']
+            return dict
+        else:
+            return 'No'
+    elif 'stories' in rest.keys():
+        if rest['stories'][0]['Type'] == 'Story-Video':
+            dict['type'] = 'story-video'
+            dict['media'] = rest['stories'][0]['media']
+            # for i in range(len(rest['stories'])):
+            #     dict['media'] = rest['stories'][i]['media']
+            return dict
+        elif rest['stories'][0]['Type'] == 'Story-Image':
+            dict['type'] = 'story-image'
+            dict['media'] = rest['stories'][0]['media']
+            # for i in range(len(rest['stories'])):
+            #     dict['media'] = rest['stories'][i]['media']
+            return dict
+    else:
+        return 'No'
+#     return rest['stories']
+#
+#
+# pp(instadownload('https://www.instagram.com/stories/sherali_12_13/3144999608330379198/'))
